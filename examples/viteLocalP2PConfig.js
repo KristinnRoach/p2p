4eb7d55f494c1563c2a43d45dcf701cd5feaf086
kiddi <kristinnroach@gmail.com>
2026-05-19 01:19:08 +0000

    Fix web components dev server aliases

--- examples/viteLocalP2PConfig.js
+++ examples/viteLocalP2PConfig.js
@@ -7,6 +7,18 @@ export function localP2PConfig(configUrl) {
     resolve: {
       alias: [
         {
+          find: /^@kidlib\/p2p\/components\/solid$/,
+          replacement: `${sourceRoot}/components/solid/index.jsx`,
+        },
+        {
+          find: /^@kidlib\/p2p\/components$/,
+          replacement: `${sourceRoot}/components/web-components.js`,
+        },
+        {
+          find: /^@kidlib\/p2p\/solid$/,
+          replacement: `${sourceRoot}/adapters/solid.js`,
+        },
+        {
           find: /^@kidlib\/p2p$/,
           replacement: `${sourceRoot}/index.js`,
         },
@@ -17,7 +29,12 @@ export function localP2PConfig(configUrl) {
       ],
     },
     optimizeDeps: {
-      exclude: ['@kidlib/p2p'],
+      exclude: [
+        '@kidlib/p2p',
+        '@kidlib/p2p/components',
+        '@kidlib/p2p/components/solid',
+        '@kidlib/p2p/solid',
+      ],
     },
     server: {
       fs: {
